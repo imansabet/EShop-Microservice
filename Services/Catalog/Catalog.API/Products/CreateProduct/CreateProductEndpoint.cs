ba324977bc81f9d4bf593df779050ce2ea9d2a26
iman <dev.imansabet@gmail.com>
2024-10-26 19:44:47 +0330

    CQRS : Add Handler Interfaces - Implement  CQRS Abstractions ICommand into Vertical Slice  Feature Handler

--- Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,6 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public class CreateProductEndpoint
 {
-    public class CreateProductEndpoint
-    {
-    }
+
 }
--- Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,18 +1,32 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand
     (string Name,List<string> Category
     ,string Description, string ImageFile
-    ,decimal Price) : IRequest<CreateProductResult>;
+    ,decimal Price) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
 
-internal class CreateProductCommandHandler() : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler() : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        //business logic to create a product
+        //Business logic to create a product
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        //Save to Db
+        //Return Result: CreateProductResult
+        return new CreateProductResult(Guid.NewGuid());
+            
     }
 }
