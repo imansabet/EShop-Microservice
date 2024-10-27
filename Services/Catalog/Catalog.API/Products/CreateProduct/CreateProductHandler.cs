74166b9f017597ea7c89f8ef24a72b387f479016
iman <dev.imansabet@gmail.com>
2024-10-27 17:45:27 +0330

    Infrastructure - Data Concern for Catalog Api .NET Transactional Db (Develop CommandHandler to Save Product to Db )

--- Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
+﻿
 using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
@@ -11,7 +10,7 @@ public record CreateProductCommand
 public record CreateProductResult(Guid Id);
 
 
-internal class CreateProductCommandHandler() : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -25,8 +24,10 @@ internal class CreateProductCommandHandler() : ICommandHandler<CreateProductComm
             Price = command.Price
         };
         //Save to Db
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
         //Return Result: CreateProductResult
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
             
     }
 }
