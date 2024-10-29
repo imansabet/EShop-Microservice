8ac7a1a6d475900d58cdbc0400689a08f8c29701
iman <dev.imansabet@gmail.com>
2024-10-29 21:04:32 +0330

    Develop Update Product (PUT)  Endpoint with Minimal Api & Carter

--- Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
+++ Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -1,6 +1,32 @@
-﻿namespace Catalog.API.Products.UpdateProduct
+﻿using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductRequest
+    (Guid Id,string Name, List<string> Category
+    , string Description, string ImageFile
+    , decimal Price);
+
+public record UpdateProductResponse(Guid Id);
+
+public class UpdateProductEndpoint : ICarterModule
 {
-    public class UpdateProductEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapPut("/products"
+            , async (UpdateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<UpdateProductResponse>();
+                return Results.Ok(response);
+
+            })
+            .WithName("UpdateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Update Product")
+            .WithDescription("Update Product");
     }
 }
