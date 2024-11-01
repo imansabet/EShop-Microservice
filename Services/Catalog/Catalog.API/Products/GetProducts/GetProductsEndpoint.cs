12d41c7405851261f5c9aeeb22ddaebb1006ea53
iman <dev.imansabet@gmail.com>
2024-11-01 13:12:15 +0330

    Develop pagination for GetProducts Query

--- Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,15 +1,18 @@
 ﻿namespace Catalog.API.Products.GetProducts;
 
-//public record GetProductRquest()
+public record GetProductRquest(int? PageNumber = 1 , int? PageSize = 10);
 public record GetProductResponse(IEnumerable<Product> Products);
 
 public class GetProductsEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async (ISender sender) =>
+        app.MapGet("/products", async ([AsParameters] GetProductRquest request, ISender sender) =>
         {
-            var result = await sender.Send(new GetProductsQuery());
+            var query = request.Adapt<GetProductsQuery>();
+
+            var result = await sender.Send(query);
+
             var response = result.Adapt<GetProductResponse>();
             return Results.Ok(response);
         })
