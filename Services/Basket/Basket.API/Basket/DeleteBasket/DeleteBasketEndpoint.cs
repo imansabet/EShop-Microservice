a678aaf8bbc3144c10fda4ab484a3f9fc3a2a1ad
iman <dev.imansabet@gmail.com>
2024-11-01 17:47:36 +0330

    Develop Delete Basket Endpoint

--- Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,6 +1,28 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+using Basket.API.Basket.GetBasket;
+
+namespace Basket.API.Basket.DeleteBasket;
+
+//public record DeleteBasketRequest(string UserName);
+public record DeleteBasketResponse(bool IsSuccess);
+
+public class DeleteBasketEndpoint : ICarterModule
 {
-    public class DeleteBasketEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapDelete("/basket/{username}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteBasketCommand(userName));
+            var response = result.Adapt<DeleteBasketResponse>();
+            return Results.Ok(response);
+        })
+           .WithName("DeleteBasket")
+           .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+           .ProducesProblem(StatusCodes.Status400BadRequest)
+           .ProducesProblem(StatusCodes.Status404NotFound)
+           .WithSummary("Delete Basket")
+           .WithDescription("Delete Basket");
+
+
     }
 }
