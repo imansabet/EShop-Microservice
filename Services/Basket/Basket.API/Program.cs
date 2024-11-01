b7cbbfa8bb13b3389d4370ec4e62a2a82d79bc2f
iman <dev.imansabet@gmail.com>
2024-11-01 22:43:02 +0330

    Register & Configure Marten DocumentDb Library

--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -12,7 +12,11 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 
 });
-
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions(); 
 
 var app = builder.Build();
 
