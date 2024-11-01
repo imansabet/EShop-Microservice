c6bf26029f6eb6c5cb33d0152163ccd0ffac0aa1
iman <dev.imansabet@gmail.com>
2024-11-01 11:05:47 +0330

    Global Handling Exceptions  (UseExceptionHandler), BuildingBlocks Handling Exception Generic way

--- Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -39,7 +39,7 @@ internal class UpdateProductCommandHandler
         var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
         if (product is null)
         {
-            throw new ProductNotFoundException();
+            throw new ProductNotFoundException(command.Id);
         }
 
         product.Name = command.Name;
--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,8 @@
 using BuildingBlocks.Behaviors;
 using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Catalog.API
 {
@@ -25,6 +28,28 @@ namespace Catalog.API
 
             var app = builder.Build();
             app.MapCarter();
+
+            app.UseExceptionHandler(exceptionHandlerApp =>
+                exceptionHandlerApp.Run(async context =>
+                {
+                    var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+                    if (exception == null)
+                    {
+                        return;
+                    }
+                    var problemDetails = new ProblemDetails
+                    {
+                        Title = exception.Message,
+                        Status = StatusCodes.Status500InternalServerError,
+                        Detail = exception.StackTrace,
+                    };
+                    var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(exception, exception.Message);
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    context.Response.ContentType = "application/problem+json";
+                    await context.Response.WriteAsJsonAsync(problemDetails);
+
+                }));
 
             app.Run();
         }
