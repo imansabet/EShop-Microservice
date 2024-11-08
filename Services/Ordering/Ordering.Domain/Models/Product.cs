aca37e8aa99b2917344f9b64823d45fe61c2bb72
iman <dev.imansabet@gmail.com>
2024-11-08 14:45:13 +0330

    Develop Strongly Typed Ids for Order Entities

--- Services/Ordering/Ordering.Domain/Models/Product.cs
+++ Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Ordering.Domain.Models;
 
-public class Product : Entity<Guid>
+public class Product : Entity<ProductId>
 {
     public string Name { get; set; } = default!;
     public decimal Price { get; set; } = default!;
