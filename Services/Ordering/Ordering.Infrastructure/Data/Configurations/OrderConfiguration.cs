d2072e3d853a5a5596efe6f04b75eec6e72178f6
iman <dev.imansabet@gmail.com>
2024-11-09 22:14:38 +0330

    Mapping DDD  Objects to Ef Entities - Develop Ef  Core Entity Configuration

---
+++ Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Enums;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        return;
+    }
+}
