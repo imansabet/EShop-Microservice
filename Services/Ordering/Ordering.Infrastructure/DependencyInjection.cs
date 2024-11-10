51eae9f450e0a7a5a2d9d55d1e1bab42fd1b0b76
iman <dev.imansabet@gmail.com>
2024-11-10 18:18:22 +0330

    Register Ef  Core DbContext

--- Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -9,8 +9,8 @@ public static class DependencyInjection
         (this IServiceCollection services,IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Database");
-        //services.AddDbContext<ApplicationDbContext>(options =>
-        // options.UseSqlServer(connectionString));
+        services.AddDbContext<ApplicationDbContext>(options =>
+         options.UseSqlServer(connectionString));
 
 
         //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
