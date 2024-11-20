2ef2535dc58b7b16cc8d714e769731b3be9c5002
iman <dev.imansabet@gmail.com>
2024-11-20 22:16:53 +0330

    Develop Service Classes for consume YarpApiGateway

---
+++ WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,13 @@
+﻿namespace Shopping.Web.Services;
+    
+public interface ICatalogService 
+{
+    Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
+
+    Task<GetProductByIdResponse> GetProduct(Guid id);
+
+    Task<GetProductByCategoryResponse> GetProductsByCategory(string category);
+
+}
+
+
